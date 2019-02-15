77b92d29c4bdf5891edaa7a9aed5cdf147d51c45
Wenish <Wenish@users.noreply.github.com>
2019-02-15 12:49:51 +0100

    adding bignumber.js

--- src/services/serviceMovePlayers.ts
+++ src/services/serviceMovePlayers.ts
@@ -1,6 +1,9 @@
-import { IStatePlayers } from "../rooms/IStatePlayers";
+import BigNumber from "bignumber.js"
+import { IStatePlayers } from "../rooms/IStatePlayers"
 
 export function movePlayers (state: IStatePlayers) {
+    var x = new BigNumber(0.1).plus(0.2)
+    console.log(x.toNumber()) //0.3
     for (let key in state.players) {
         //TODO move player to moveTo position
         // state.players[key]
