a14419e07e8768db23117b1551aed2b4e4cf600f
Wenish <Wenish@users.noreply.github.com>
2019-03-06 14:09:38 +0100

    updated base move speec

--- src/models/player.ts
+++ src/models/player.ts
@@ -39,6 +39,6 @@ export class Player {
         this.id = id
         this.position = new Position(0, 0, 0)
         this.moveTo = new Position(10,0,0)
-        this.moveSpeed = 60
+        this.moveSpeed = 300
     }
 }
