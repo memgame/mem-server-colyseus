5a5f4eedb346c913dde033d0f4b1504c148db24a
Wenish <wenish@noreply.github.com>
2019-04-04 18:00:32 +0200

    added error handling

--- src/actions/actionTargetPlayer.ts
+++ src/actions/actionTargetPlayer.ts
@@ -1,9 +1,23 @@
 import { Action } from "./index";
 import { IStateRoot } from "../states/StateRoot";
 import { Client } from "colyseus";
+import { TARGET_PLAYER } from "./actionTypes";
 
 export const actionTargetPlayer: Action<IStateRoot, Client> = ({statePlayers}, {sessionId}, payload) => {
-    var player = statePlayers.players[sessionId]
-    var target = statePlayers.players[payload.playerId]
-    player.target = target
+    try {
+        if(!payload) throw 'payload is not defined'
+        if(!payload.playerId) throw 'payload.playerId is not defined'
+
+        var target = statePlayers.players[payload.playerId]
+
+        if(!target) throw 'could not find target'
+
+        var player = statePlayers.players[sessionId]
+
+        if(!player) throw 'could not find player'
+
+        player.target = target
+    } catch (err) {
+        console.error('Error', TARGET_PLAYER, err)
+    }
 }
