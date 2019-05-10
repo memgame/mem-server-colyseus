3869e3eb75a8b43c8c2ca8aa31b7e49d7cb55727
Wenish <wenish@noreply.github.com>
2019-05-10 13:00:57 +0200

    add weapon model

---
+++ src/models/weapon.ts
@@ -0,0 +1,35 @@
+import { Schema, type } from "@colyseus/schema";
+
+export class Weapon extends Schema {
+    @type('string')
+    public weaponType: WeaponType
+
+    @type('string')
+    public weaponSlot: WeaponSlot
+
+    @type('string')
+    public combatStyle: CombatStyle
+
+    @type('number')
+    public attackRange: number
+}
+
+enum WeaponType {
+    Axe = 'Axe',
+    Sword = 'Sword',
+    Spear = 'Spear',
+    Bow = 'Bow',
+    Daggers = 'Daggers',
+    Shield = 'Shield',
+}
+
+enum WeaponSlot {
+    OneHanded = 'OneHanded',
+    TwoHanded = 'TwoHanded',
+    OffHand = 'OffHand'
+}
+
+enum CombatStyle {
+    Melee = 'melee',
+    Ranged = 'Ranged'
+}
