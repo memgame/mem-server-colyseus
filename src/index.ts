be61df89e693143e25be806dabccb27ebe6579c2
Wenish <wenish@noreply.github.com>
2019-07-03 10:42:11 +0200

    added colyseus monitor

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from 'cors';
 import health from "express-ping";
 import { createServer } from 'http';
 import { Server } from 'colyseus';
+import { monitor } from '@colyseus/monitor';
 
 // Import Rooms
 import { Match } from "./rooms/match";
 
 const port = Number(process.env.PORT || 8080);
 const app = express();
+
+app.use(cors());
 
 // Create HTTP Server
 const httpServer = createServer(app);
@@ -24,11 +28,12 @@ const gameServer = new Server({
     }
 });
 
-// TODO make this configurable for prod
-app.use(health.ping())
-
 // Register Rooms
 gameServer.register("match", Match);
+
+// TODO make this configurable for prod
+app.use(health.ping())
+app.use("/colyseus", monitor(gameServer));
 
 gameServer.listen(port);
 
