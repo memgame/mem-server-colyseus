6329c41db9ba8b81d8c710c89062e6a33c04bba6
Wenish <wenish@noreply.github.com>
2019-07-04 12:43:42 +0200

    update

--- src/entities/baseEntitiy.ts
+++ src/entities/baseEntitiy.ts
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 import { Schema, type } from "@colyseus/schema";
 
-export class BaseEntity extends Schema {
+export abstract class BaseEntity extends Schema {
   @type('string') id: string;
   @type('string') type: EnitityType;
   
