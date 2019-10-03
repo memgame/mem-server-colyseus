0170d2b64f91e64447f3fd1c0f5bcf354c4fd155
Wenish <wenish@noreply.github.com>
2019-10-03 10:03:22 +0200

    update

--- src/actions/actionMoveUnitTo.ts
+++ src/actions/actionMoveUnitTo.ts
@@ -15,6 +15,10 @@ import { Unit } from "../entities/unit";
 */
 export const actionMoveUnitTo: Action<IStateRoot, Client> =  (room, { statePlayers, stateUnits }, { sessionId }, payload) => {
     //TODO check payload
+    if(!payload) throw 'payload is not defined'
+    if(!payload.x) throw 'payload.x is not defined'
+    if(!payload.z) throw 'payload.z is not defined'
+
     const player: Player = statePlayers.players[sessionId]
     if(!player) throw 'could not find player'
     if(!player.unitId) throw 'no unit id in player'
